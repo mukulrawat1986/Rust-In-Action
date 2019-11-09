edda429ac32d2ca033d02f4cb0d3e96ad1349ad2
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-09 16:27:28 +0530

    using the match keyword on multiple values

---
+++ match_needles/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+//    let needle = 42;
+    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+
+    for reference in haystack.iter() {
+        let item = *reference;
+
+        let result = match item {
+            42| 132 => "hit!",
+            _ => "miss",
+        };
+
+        if result == "hit!" {
+            println!("{}, {}", item, result);
+        }
+    }
+}
